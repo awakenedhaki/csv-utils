f03e034ca61196ede9ef75b8ee3b4b46ed7f1b23
awakenedhaki <rodrigo.vallejosch@gmail.com>
2019-08-27 18:42:17 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="split",
+    version="0.1.0",
+    url="https://github.com/awakenedhaki/split",
+    license="MIT",
+    author="awakenedhaki",
+    author_email="",
+    description="Splits CSV files into many with a given number of rows.",
+    python_requires=">=3",
+    install_requires=[
+        "click"
+    ]
+)
