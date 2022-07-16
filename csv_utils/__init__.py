8f3d32e1dad3d3f2e5ea10f07ec5c31476e018ee
awakenedhaki <rodrigo.vallejosch@gmail.com>
2022-07-15 23:36:11 -0700

    Correcting module imports

--- csv_utils/__init__.py
+++ csv_utils/__init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.1.0'
 __author__ = 'Rodrigo Vallejos'
 
-from load_tables import load_tables
-from manipulations import *
-from save_ import *
+from csv_utils.load_tables import *
+from csv_utils.manipulations import *
+from csv_utils.save_ import *
 
 
