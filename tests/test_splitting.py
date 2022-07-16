4a8e584d9fbf96fa179fb5a6ea35d8fb17958c44
awakenedhaki <rodrigo.vallejosch@gmail.com>
2022-07-16 15:50:08 -0700

    Creating test_splitting.py

---
+++ tests/test_splitting.py
@@ -0,0 +1,20 @@
+from csv_utils import split_, split_ntables
+from tests.utils import read_sample, generate_lines
+
+def test_split_single_column_with_round_number_rows(read_sample, generate_lines):
+    first_expected = read_sample('03', 'csv')
+    second_expected = read_sample('04', 'csv')
+
+    sample = generate_lines(10)
+    first, second = [list(output) for output in split_ntables(sample, ntables = 2)]
+    assert (first == first_expected) & (second == second_expected)
+    
+def test_split_single_column_with_non_round_number_rows(read_sample, generate_lines):
+    first_expected = read_sample('03', 'csv')
+    second_expected = read_sample('04', 'csv')
+    third_expected = read_sample('05', 'csv')
+
+    sample = generate_lines(11)
+    first, second, third = [list(output) for output in split_ntables(sample, ntables = 2)]
+    assert (first == first_expected) & (second == second_expected) & (third == third_expected)
+    
