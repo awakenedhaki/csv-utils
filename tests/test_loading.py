1fb06a9e3bf62222ca4e4bc2135e2900a4d2c1b1
awakenedhaki <rodrigo.vallejosch@gmail.com>
2022-07-17 16:46:06 -0700

    Removing fixtures

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -8,24 +8,25 @@ from tests.utils import DATA_DIR, read_sample
     '01',
     '02'
 ])
-def test_load_table_csv_one_column(sample_id, read_sample):
+def test_load_table_csv_one_column(sample_id):
     expected = read_sample(sample_id, 'csv')
     path_sample = os.path.join(DATA_DIR, 'sample', f'sample-{sample_id}.csv')
     observed = next(load_table(path_sample))
     assert observed == expected
 
-pytest.mark.parametrize('sample_ids', [
+pytest.mark.parametrize('first,second,file_type', [
     ['01', '02', 'csv'],
-    ['02', '01', 'csv']
+    ['02', '01', 'csv'],
     ['01', '02', 'tsv'],
     ['02', '01', 'tsv']
 ])
-def test_load_tables_csv_two_columns(sample_ids, read_sample):
-    file_type = sample_ids.pop()
-    expected = [read_sample(sample_ids[0], file_type), read_sample(sample_ids[1], file_type)]
-    
-    sample_paths = []
-    for id in sample_ids:
-        sample_paths.append(os.path.join(DATA_DIR, 'sample', f'sample-{id}.{file_type}'))
+def test_load_tables_csv_two_columns(first, second, file_type):
+    expected = [
+        read_sample(first, file_type),
+        read_sample(second, file_type)
+    ]
+    sample_paths = [
+        os.path.join(DATA_DIR, 'sample', f'sample-{id}.{file_type}') for id in [first, second]
+    ]
     observed = [sample for sample in load_tables(sample_paths)]
     assert observed == expected
--- tests/test_splitting.py
+++ tests/test_splitting.py
@@ -1,7 +1,7 @@
 from csv_utils import split_, split_ntables
 from tests.utils import read_sample, generate_lines
 
-def test_split_single_column_with_round_number_rows(read_sample, generate_lines):
+def test_split_single_column_with_round_number_rows():
     first_expected = read_sample('03', 'csv')
     second_expected = read_sample('04', 'csv')
 
@@ -9,7 +9,7 @@ def test_split_single_column_with_round_number_rows(read_sample, generate_lines)
     first, second = [list(output) for output in split_ntables(sample, ntables = 2)]
     assert (first == first_expected) & (second == second_expected)
     
-def test_split_single_column_with_non_round_number_rows(read_sample, generate_lines):
+def test_split_single_column_with_non_round_number_rows():
     first_expected = read_sample('03', 'csv')
     second_expected = read_sample('04', 'csv')
     third_expected = read_sample('05', 'csv')
